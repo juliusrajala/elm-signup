7c97f668e30313843d3755d9624c7e4c23ac121e
Julius Rajala <juliusrajala@gmail.com>
2018-01-30 20:37:18 +0200

    Changed form-contents, rebuilt model altered css and dom structure.

--- src/Main.elm
+++ src/Main.elm
@@ -1,6 +1,6 @@
 module Main exposing (..)
 
-import Html exposing (Html, Attribute, text, div, h1, p, img, form, input, label, button)
+import Html exposing (Html, Attribute, text, div, h1, p, img, form, input, label, button, span)
 import Html.Attributes exposing (src, class, type_)
 import Html.Events exposing (onWithOptions, onInput)
 
@@ -24,6 +24,8 @@ type alias Model =
     , firstname : String
     , lastname : String
     , organization : String
+    , password : String
+    , repeatPassword : String
     , validEmail : Bool
     }
 
@@ -33,12 +35,19 @@ initialState =
     , firstname = ""
     , lastname = ""
     , organization = ""
+    , password = ""
+    , repeatPassword = ""
     , validEmail = False
     }
 
 init : ( Model, Cmd Msg )
 init =
     ( initialState , Cmd.none )
+
+isPasswordValid : String -> String -> Bool
+isPasswordValid password repeatPassword =
+    password == repeatPassword && String.length password > 5
+
 
 
 
@@ -57,6 +66,9 @@ type Msg
     | ChangeFirstname String
     | ChangeLastname String
     | ChangeOrganization String
+    | ChangePassword String
+    | ChangeRepeatPassword String
+    | NoOp
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -70,7 +82,11 @@ update msg model =
             ( { model | lastname = lastname }, Cmd.none )
         ChangeOrganization organization ->
             ( { model | organization = organization }, Cmd.none )
-
+        ChangePassword password ->
+            ( { model | password = password }, Cmd.none )
+        ChangeRepeatPassword repeatPassword ->
+            ( { model | repeatPassword = repeatPassword }, Cmd.none )
+        NoOp -> ( model, Cmd.none )
 
 ---- VIEW ----
 
@@ -81,20 +97,57 @@ view model =
         [ signupForm model
         ]
 
-signupForm : Model -> Html Msg
-signupForm model =
-    div [ class "SignupContainer" ]
-        [ h1 [ class "SignupContainer-title" ] [ text formTitle ]
-        , p [ class "SignupContainer-description" ]
-            [ text welcomeMessage ]
-        , form [ class "SignupForm" ]
-            [ label [ class "Input-label", onInput ChangeEmail ] [ text "Email", input [ class "Input-field" ] [] ]
-            , label [ class "Input-label", onInput ChangeFirstname ] [ text "Firstname", input [ class "Input-field" ] [] ]
-            , label [ class "Input-label", onInput ChangeLastname ] [ text "Lastname", input [ class "Input-field" ] [] ]
-            , label [ class "Input-label", onInput ChangeOrganization ] [ text "Organization", input [ class "Input-field" ] [] ]
-            , button [ class "SignupForm-button" ] [ text "Next" ]
+formNavigation : Model -> Html Msg
+formNavigation model =
+    div [ class "FormNavigation" ]
+        [ div [ class "FormNavigation-item" ]
+            [ span [ class "FormNavigation-box" ] []
+            , span [ class "FormNavigation-text" ] [ text "Account" ]
+            ]
+        , div [ class "FormNavigation-item" ]
+            [ span [ class "FormNavigation-box" ] []
+            , span [ class "FormNavigation-text" ] [ text "User info" ]
+            ]
+        , div [ class "FormNavigation-item" ]
+            [ span [ class "FormNavigation-box" ] []
+            , span [ class "FormNavigation-text" ] [ text "Confirmation" ]
             ]
         ]
+
+signupForm : Model -> Html Msg
+signupForm model =
+    let
+        validPassword : Bool
+        validPassword = isPasswordValid model.password model.repeatPassword
+
+        passwordClasses : String
+        passwordClasses =
+            if validPassword then
+                "Input-field Input-field--Valid"
+            else
+                "Input-field"
+        
+        nextButton : Html Msg
+        nextButton =
+            if validPassword then
+                button [ class "SignupForm-button" ] [ text "Next" ]
+            else
+                button [ class "SignupForm-button" ] [ text "Fix" ]
+    in
+        div [ class "SignupContainer" ]
+            [ formNavigation model
+            , div [ class "SignupContainer-form" ]
+                [ h1 [ class "SignupContainer-title" ] [ text formTitle ]
+                , form [ class "SignupForm" ]
+                    [ p [ class "SignupContainer-description" ] [ text welcomeMessage ]
+                    , label [ class "Input-label", onInput ChangeEmail ] [ text "Email", input [ class "Input-field" ] [] ]
+                    , label [ class "Input-label", onInput ChangePassword ] [ text "Password", input [ class passwordClasses, type_ "password"  ] [] ]
+                    , label [ class "Input-label", onInput ChangeRepeatPassword ] [ text "Repeat password", input [ class passwordClasses, type_ "password"  ] [] ]
+                    , nextButton
+                    ]
+                ]
+            ]
+            
 
 ---- PROGRAM ----
 
