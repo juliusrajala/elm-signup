d2715dc983849d9dbcd7b46774b3eefd3ccf2257
Julius Rajala <juliusrajala@gmail.com>
2018-03-05 19:54:18 +0200

    Adjusted forms to clear reused inputs in the DOM.

--- src/Views/AccountForm.elm
+++ src/Views/AccountForm.elm
@@ -1,8 +1,8 @@
 module Views.AccountForm exposing (..)
 
 import Types exposing (Model, Msg)
-import Html exposing (Html, Attribute, text, div, h1, p, img, form, input, label, button, span)
-import Html.Attributes exposing (src, class, type_)
+import Html exposing (Html, Attribute, text, p, img, form, input, label)
+import Html.Attributes exposing (src, class, type_, value)
 import Html.Events exposing (onInput)
 
 import Utils.Strings exposing (formTitle, welcomeMessage)
@@ -12,7 +12,10 @@ accountForm : Model -> Html Msg
 accountForm model =
     form [ class "SignupForm" ]
         [ p [ class "SignupContainer-description" ] [ text welcomeMessage ]
-        , label [ class "Input-label", onInput (Types.MsgForAccount << Types.ChangeEmail) ] [ text "Email", input [ class "Input-field" ] [] ]
-        , label [ class "Input-label", onInput (Types.MsgForAccount << Types.ChangePassword) ] [ text "Password", input [ class "Input-field", type_ "password"  ] [] ]
-        , label [ class "Input-label", onInput (Types.MsgForAccount << Types.ChangeRepeatPassword) ] [ text "Repeat password", input [ class "Input-field", type_ "password"  ] [] ]
+        , label [ class "Input-label", onInput (Types.MsgForAccount << Types.ChangeEmail) ]
+            [ text "Email", input [ class "Input-field" , value model.email] [] ]
+        , label [ class "Input-label", onInput (Types.MsgForAccount << Types.ChangePassword) ]
+            [ text "Password", input [ class "Input-field", type_ "password", value model.password ] [] ]
+        , label [ class "Input-label", onInput (Types.MsgForAccount << Types.ChangeRepeatPassword) ]
+            [ text "Repeat password", input [ class "Input-field", type_ "password", value model.repeatPassword ] [] ]
         ]
--- src/Views/DetailsForm.elm
+++ src/Views/DetailsForm.elm
@@ -1,15 +1,18 @@
 module Views.DetailsForm exposing (..)
 
 import Types exposing (Model, Msg)
-import Html exposing (Html, Attribute, text, div, h1, p, img, form, input, label, button, span)
-import Html.Attributes exposing (src, class, type_)
+import Html exposing (Html, text, p, form, input, label, button)
+import Html.Attributes exposing (src, class, type_, value)
 import Html.Events exposing (onInput)
 
 detailsForm : Model -> Html Msg
 detailsForm model =
     form [ class "SignupForm" ]
         [ p [ class "SignupContainer-description" ] [ text "General information about account owner." ]
-        , label [ class "Input-label", onInput (Types.MsgForDetails << Types.ChangeOrganization) ] [ text "Organization", input [ class "Input-field" ] [] ]
-        , label [ class "Input-label", onInput (Types.MsgForDetails << Types.ChangeFirstname) ] [ text "Firstname", input [ class "Input-field" ] [] ]
-        , label [ class "Input-label", onInput (Types.MsgForDetails << Types.ChangeLastname) ] [ text "Lastname", input [ class "Input-field" ] [] ]
+        , label [ class "Input-label", onInput (Types.MsgForDetails << Types.ChangeOrganization) ]
+            [ text "Organization", input [ class "Input-field", value model.organization ] [] ]
+        , label [ class "Input-label", onInput (Types.MsgForDetails << Types.ChangeFirstname) ]
+            [ text "Firstname", input [ class "Input-field", value model.firstName ] [] ]
+        , label [ class "Input-label", onInput (Types.MsgForDetails << Types.ChangeLastname) ]
+            [ text "Lastname", input [ class "Input-field", value model.lastName ] [] ]
         ]
